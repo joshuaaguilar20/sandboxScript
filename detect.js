f21acd676e2a841478181ad37e13ff7bdc2c12a2
Joshua Aguilar <joshuaaguilar20@gmail.com>
2019-04-09 12:43:49 -0700

    Update detect.js

--- detect.js
+++ detect.js
@@ -9,6 +9,63 @@ function isSandboxedIframe() {
   return f.hasAttribute('sandbox') ? 'sandboxed' : 'non-sandboxed';
 }
 
+
+
+var flashVPaid = new VPAIDFLASHClient(element, flashVPAIDWrapperLoaded);
+function flashVPAIDWrapperLoaded(err, result) {
+    if (err) {
+        //handle error here
+        return;
+    }
+
+    flashVPaid.loadAdUnit('TestAd.swf', function (error, adUnit) {
+
+        if (err) {
+            //handle error here
+            return;
+        }
+
+        adUnit.on('AdLoaded', function (err, result) {
+            console.log('event:AdLoaded', err, result);
+            startAd();
+        });
+
+        adUnit.on('AdStarted', function (err, result) {
+            console.log('event:AdStarted', err, result);
+            checkAdProperties();
+        });
+
+        adUnit.handshakeVersion('2.0', function (err, result) {
+            initAd();
+        });
+
+        function initAd() {
+            adUnit.initAd(0, 0, 'normal', -1, '', '', function (err) {
+                console.log('initAd', err);
+            });
+        }
+
+        function startAd() {
+            adUnit.startAd(function (err, result) {
+                console.log('startAd call', err, result);
+            });
+        }
+
+        function checkAdProperties() {
+            adUnit.getAdIcons(function (err, result) {
+                console.log('getAdIcons', result);
+            });
+            adUnit.setAdVolume(10, function (err, result) {
+                console.log('setAdVolume', result);
+            });
+            adUnit.getAdVolume(function (err, result) {
+                console.log('getAdVolume', result);
+            });
+        }
+
+    });
+}
+
 setInterval(isSandboxedIframe, 3000);
 
  
